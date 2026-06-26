5ebe2e7a93827b2ac426a9982d244fe848a3be71
Sahildevta30 <suryasahilsskg@gmail.com>
2026-06-26 06:20:02 +0530

    Fix inventory routes

--- src/routes/inventoryRoutes.js
+++ src/routes/inventoryRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const ic = require("../controllers/inventoryController");
-const sc = require("../controllers/settingsController");
 const protect = require("../middleware/authMiddleware");
 const admin = require("../middleware/adminMiddleware");
 
